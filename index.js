f88f91af5e940d7b0af97484cbf7275a79e088f9
SDFIW <asdfo2@SDFIWs-MacBook-Pro.local>
2020-03-06 00:29:46 -0500

    initial work for the server

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const axios = require("axios")
+require("dotenv").config();
+// const db = require("./express-server/db/sequelize-init.js");
+// Middleware
+let cors = require("cors");
+const morgan = require("morgan");
+const parser = require("body-parser");
+// Router
+const router = require("./routes");
+// Server Instantiation
+const app = express();
+app.set("port", process.env.PORT || 3000);
+
+// Logging and parsing
+app.use(cors());
+app.use(morgan("dev"));
+app.use(parser.urlencoded({ extended: true }));
+app.use(parser.json());
+// Set up our routes
+app.get("/", (req,res)=>{
+  axios.get("https://api.yelp.com/v3/businesses/search",{
+    headers:{
+      Authorization: `Bearer ${process.env.REACT_APP_YELP_KEY}`
+    },
+    params:req.query
+  })
+  .then((results)=>{
+    res.send(results.data.businesses)
+  })
+  .catch((err)=>{
+    console.log(err)
+  })
+});
+
+app.listen(app.get("port"), () => {
+  console.log("Listening on", app.get("port"));
+});
