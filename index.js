c60bd46cae6c3c34ac75a60188db20aa9dc936e3
SDFIW <asdfo2@SDFIWs-MacBook-Pro.local>
2020-03-07 16:28:07 -0500

    cleaned up code

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
 const express = require("express");
 const axios = require("axios")
 require("dotenv").config();
-// const db = require("./express-server/db/sequelize-init.js");
-// Middleware
+
 let cors = require("cors");
 const morgan = require("morgan");
 const parser = require("body-parser");
-// Router
-const router = require("./routes");
-// Server Instantiation
+
 const app = express();
 app.set("port", process.env.PORT || 3001);
 
-// Logging and parsing
 app.use(cors());
 app.use(morgan("dev"));
 app.use(parser.urlencoded({ extended: true }));
 app.use(parser.json());
-// Set up our routes
+
 app.get("/", (req,res)=>{
   axios.get("https://api.yelp.com/v3/businesses/search",{
     headers:{
